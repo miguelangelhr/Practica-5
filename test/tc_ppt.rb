80a11fe9e7bcd7d4b383eaa2ba591d13400c7ffa
miguelangelhr <miguelpive10@gmail.com>
2012-10-21 14:00:02 +0100

    mejora de TestUnit

--- test/tc_ppt.rb
+++ test/tc_ppt.rb
@@ -7,38 +7,42 @@
 require 'ppt'
 require 'test/unit'
 
-def setup
-	@ppt_obj = PiedraPapelTijera.new("tijera")
-end
+class TestPiedraPapelTijera < Test::Unit::TestCase
 
-def test_jugar()
-
-	@ppt_obj.obtener_jugador('piedra')
-	marcador = []
-
-	30.times do
-		marcador.push(@ppt_obj.jugar())
+	def setup
+		@ppt_obj = PiedraPapelTijera.new("tijera")
 	end
 
-	assert(marcador.uniq.length >= 3, "---> No se han obtenido los 3 resultados esperados --> marcador.uniq.length = #{marcador.uniq.length}")
+	def test_jugar()
 
-end
+		@ppt_obj.obtener_jugador('piedra')
+		marcador = []
 
-def test_variado()
+		30.times do
+			marcador.push(@ppt_obj.jugar())
+		end
 
-	@ppt_obj = obtener_jugador(@ppt_obj.tiradas.sample.to_s)
-	respuestas = []
+		assert(marcador.uniq.length >= 3, "---> No se han obtenido los 3 resultados esperados --> marcador.uniq.length = #{marcador.uniq.length}")
 
-	30.times do
-		respuestas.push(@ppt_obj.jugar())
 	end
 
-	assert(respuestas.uniq.legth >= 3, "---> No se han obtenido los 3 tipos de respuestas esperados")
+	def test_variado()
 
-end
+		@ppt_obj = obtener_jugador(@ppt_obj.tiradas.sample.to_s)
+		respuestas = []
 
-def test_erroneo()
+		30.times do
+			respuestas.push(@ppt_obj.jugar())
+		end
 
-	assert(TypeError) {@ppt_obj.obtener_humano("caja")}
+		assert(respuestas.uniq.legth >= 3, "---> No se han obtenido los 3 tipos de respuestas esperados")
+
+	end
+
+	def test_erroneo()
+
+		assert(TypeError) {@ppt_obj.obtener_humano("caja")}
+
+	end
 
 end
