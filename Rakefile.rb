8ff6a0213040e4be6cea450cda0b3b2be64eea0f
miguelangelhr <miguelpive10@gmail.com>
2012-10-21 14:54:28 +0100

    Mejora de Test-Unit

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,13 +5,19 @@ task :test do
   sh "ruby -Ilib test/tc_ppt.rb"
 end
 
+desc "Ejecuta ppt.rb con un test especifico"
+task :t, :test_name do [t, args]
+	test_name = args[:test_name] || "test_jugar"
+	sh "ruby -w -Ilib test/tc_ppt.rb --name #{test_name}"
+end
+
+desc "Ejecuta el programa principal"
+task :principal do
+	sh "ruby -Ilib bin/ppt_main.rb"
+end
+
 desc "Limpieza de temporales"
 task :limpiar do
   sh "rm -fr *.gz *~ *swp"
 end
 
-desc "Ejecuta bin/ppt_main.rb"
-task :t, :test_name do [t, args]
-	test_name = args[:test_name] || "test_play"
-	sh "ruby -w -Ilib test/tc_ppt.rb --name #[test_name]"
-end
